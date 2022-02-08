f2385f6e69eb02eb3ed675e7b68f374e1bd4be83
kilroy_lyk <736587340@qq.com>
2022-02-08 20:02:32 +0800

    KILROY

--- _KilroyProject/_Project/_Demo/Pixi/application/interface/public.ts
+++ _KilroyProject/_Project/_Demo/Pixi/application/interface/public.ts
@@ -1,3 +1,7 @@
+//----- 枚举 Start -----//
+//----- 枚举 End -----//
+
+//----- 接口 Start -----//
 export interface Vector2 { // 二维向量
     x: number, // X轴
     y: number // Y轴
@@ -8,3 +12,5 @@ export interface Vector3 { // 三维向量
     y: number, // Y轴
     z: number // Z轴
 }
+
+//----- 接口 End -----//
